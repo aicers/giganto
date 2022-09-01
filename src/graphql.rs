a2329ff5a3aaa61fd64003f4533d2c96f1c388d6
Junggon Kim <jgkim@einsis.com>
2022-09-01 12:45:07 +0900

    Add graphql log, modify conn
    Add src_raw_events to given source
    
    Close #35, Close #37

--- src/graphql.rs
+++ src/graphql.rs
@@ -37,17 +37,42 @@ impl From<ingestion::Conn> for ConnRawEvents {
 
 #[Object]
 impl Query {
-    pub async fn conn_raw_events<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<ConnRawEvents>> {
+    pub async fn conn_raw_events<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        source: String,
+    ) -> Result<Vec<ConnRawEvents>> {
         let mut raw_vec = Vec::new();
         let db = match ctx.data::<Database>() {
             Ok(r) => r,
             Err(e) => bail!("{:?}", e),
         };
-        for raw_data in db.conn_store()?.all_raw_event() {
+        for raw_data in db.conn_store()?.src_raw_events(&source) {
             let de_conn = bincode::deserialize::<ingestion::Conn>(&raw_data)?;
             raw_vec.push(ConnRawEvents::from(de_conn));
         }
 
+        Ok(raw_vec)
+    }
+
+    pub async fn log_raw_events<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        source: String,
+        kind: String,
+    ) -> Result<Vec<String>> {
+        let mut raw_vec = Vec::new();
+        let db = match ctx.data::<Database>() {
+            Ok(r) => r,
+            Err(e) => bail!("{:?}", e),
+        };
+        for raw_data in db.log_store()?.src_raw_events(&source) {
+            let de_log = bincode::deserialize::<ingestion::Log>(&raw_data)?;
+            let (k, r) = de_log.log;
+            if k == kind {
+                raw_vec.push(base64::encode(r));
+            }
+        }
         Ok(raw_vec)
     }
 }
