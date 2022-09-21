d7bcc30d49d8830386ff68c889adc59dcd59fca7
soojlee0106 <soojlee0106@einsis.com>
2022-09-21 09:17:21 +0900

    Add cursor for all queries
    Closes: #83

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,10 +1,8 @@
 mod log;
 mod network;
 
-use crate::storage::{Database, RawEventStore};
-use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, Result};
-use serde::de::DeserializeOwned;
-use std::fmt::Debug;
+use crate::storage::Database;
+use async_graphql::{EmptyMutation, EmptySubscription, MergedObject};
 
 #[derive(Default, MergedObject)]
 pub struct Query(log::LogQuery, network::NetworkQuery);
@@ -17,19 +15,6 @@ pub enum PagingType {
 }
 
 pub type Schema = async_graphql::Schema<Query, EmptyMutation, EmptySubscription>;
-
-fn response_raw_events<'a, T, K>(source: &str, store: &RawEventStore<'a>) -> Result<Vec<K>>
-where
-    T: Debug + DeserializeOwned,
-    K: From<T>,
-{
-    let mut raw_vec = Vec::new();
-    for raw_data in store.src_raw_events(source) {
-        let de_data = bincode::deserialize::<T>(&raw_data)?;
-        raw_vec.push(K::from(de_data));
-    }
-    Ok(raw_vec)
-}
 
 pub fn schema(database: Database) -> Schema {
     Schema::build(Query::default(), EmptyMutation, EmptySubscription)
--- src/graphql/network.rs
+++ src/graphql/network.rs
@@ -1,9 +1,15 @@
-use crate::{ingestion, storage::Database};
-use async_graphql::{Context, Object, Result, SimpleObject};
+use crate::{
+    ingestion,
+    storage::{gen_key, Database},
+};
+use async_graphql::{
+    connection::{query, Connection, Edge},
+    Context, Object, Result, SimpleObject,
+};
 use chrono::{DateTime, Utc};
 use std::fmt::Debug;
 
-use super::response_raw_events;
+use super::PagingType;
 
 #[derive(Default)]
 pub(super) struct NetworkQuery;
@@ -120,44 +126,216 @@ impl NetworkQuery {
         &self,
         ctx: &Context<'ctx>,
         source: String,
-    ) -> Result<Vec<ConnRawEvent>> {
-        let db = ctx.data::<Database>()?;
-        response_raw_events::<ingestion::Conn, ConnRawEvent>(&source, &db.conn_store()?)
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<String, ConnRawEvent>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                load_paging_type_conn(ctx, &source, after, before, first, last)
+            },
+        )
+        .await
     }
 
+    #[allow(clippy::too_many_arguments)]
     async fn dns_raw_events<'ctx>(
         &self,
         ctx: &Context<'ctx>,
         source: String,
         start: DateTime<Utc>,
         end: DateTime<Utc>,
-    ) -> Result<Vec<DnsRawEvent>> {
-        let mut raw_vec = Vec::new();
-        let db = ctx.data::<Database>()?;
-
-        for raw_data in db.dns_store()?.dns_time_events(&source, &start, &end) {
-            let de_dns = bincode::deserialize::<ingestion::DnsConn>(&raw_data)?;
-            raw_vec.push(DnsRawEvent::from(de_dns));
-        }
-
-        Ok(raw_vec)
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<String, DnsRawEvent>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                load_paging_type_dns(ctx, &source, start, end, after, before, first, last)
+            },
+        )
+        .await
     }
 
     async fn http_raw_events<'ctx>(
         &self,
         ctx: &Context<'ctx>,
         source: String,
-    ) -> Result<Vec<HttpRawEvent>> {
-        let db = ctx.data::<Database>()?;
-        response_raw_events::<ingestion::HttpConn, HttpRawEvent>(&source, &db.http_store()?)
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<String, HttpRawEvent>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                load_paging_type_http(ctx, &source, after, before, first, last)
+            },
+        )
+        .await
     }
 
     async fn rdp_raw_events<'ctx>(
         &self,
         ctx: &Context<'ctx>,
         source: String,
-    ) -> Result<Vec<RdpRawEvent>> {
-        let db = ctx.data::<Database>()?;
-        response_raw_events::<ingestion::RdpConn, RdpRawEvent>(&source, &db.rdp_store()?)
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<i32>,
+        last: Option<i32>,
+    ) -> Result<Connection<String, RdpRawEvent>> {
+        query(
+            after,
+            before,
+            first,
+            last,
+            |after, before, first, last| async move {
+                load_paging_type_rdp(ctx, &source, after, before, first, last)
+            },
+        )
+        .await
     }
+}
+
+fn check_paging_type(
+    after: Option<String>,
+    before: Option<String>,
+    first: Option<usize>,
+    last: Option<usize>,
+) -> anyhow::Result<PagingType> {
+    if let Some(val) = first {
+        if let Some(cursor) = after {
+            return Ok(PagingType::AfterFirst(cursor, val));
+        }
+        return Ok(PagingType::First(val));
+    }
+    if let Some(val) = last {
+        if let Some(cursor) = before {
+            return Ok(PagingType::BeforeLast(cursor, val));
+        }
+        return Ok(PagingType::Last(val));
+    }
+    Err(anyhow::anyhow!("Invalid paging type"))
+}
+
+fn load_paging_type_conn(
+    ctx: &Context<'_>,
+    source: &str,
+    after: Option<String>,
+    before: Option<String>,
+    first: Option<usize>,
+    last: Option<usize>,
+) -> Result<Connection<String, ConnRawEvent>> {
+    let db = ctx.data::<Database>()?;
+    let paging_type = check_paging_type(after, before, first, last)?;
+
+    let args: Vec<Vec<u8>> = vec![source.as_bytes().to_vec()];
+    let source = String::from_utf8(gen_key(args))?;
+
+    let (conn, prev, next) = db.conn_store()?.conn_events(&source, paging_type);
+    let mut connection: Connection<String, ConnRawEvent> = Connection::new(prev, next);
+    for conn_data in conn {
+        let (key, raw_data) = conn_data;
+        let de_conn = bincode::deserialize::<ingestion::Conn>(&raw_data)?;
+        connection
+            .edges
+            .push(Edge::new(base64::encode(key), ConnRawEvent::from(de_conn)));
+    }
+    Ok(connection)
+}
+
+#[allow(clippy::too_many_arguments)]
+fn load_paging_type_dns(
+    ctx: &Context<'_>,
+    source: &str,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+    after: Option<String>,
+    before: Option<String>,
+    first: Option<usize>,
+    last: Option<usize>,
+) -> Result<Connection<String, DnsRawEvent>> {
+    let db = ctx.data::<Database>()?;
+    let paging_type = check_paging_type(after, before, first, last)?;
+
+    let args: Vec<Vec<u8>> = vec![source.as_bytes().to_vec()];
+    let source = String::from_utf8(gen_key(args))?;
+
+    let (dns, prev, next) = db
+        .dns_store()?
+        .dns_time_events(&source, &start, &end, paging_type);
+    let mut connection: Connection<String, DnsRawEvent> = Connection::new(prev, next);
+    for dns_data in dns {
+        let (key, raw_data) = dns_data;
+        let de_dns = bincode::deserialize::<ingestion::DnsConn>(&raw_data)?;
+        connection
+            .edges
+            .push(Edge::new(base64::encode(key), DnsRawEvent::from(de_dns)));
+    }
+    Ok(connection)
+}
+
+fn load_paging_type_http(
+    ctx: &Context<'_>,
+    source: &str,
+    after: Option<String>,
+    before: Option<String>,
+    first: Option<usize>,
+    last: Option<usize>,
+) -> Result<Connection<String, HttpRawEvent>> {
+    let db = ctx.data::<Database>()?;
+    let paging_type = check_paging_type(after, before, first, last)?;
+
+    let args: Vec<Vec<u8>> = vec![source.as_bytes().to_vec()];
+    let source = String::from_utf8(gen_key(args))?;
+
+    let (http, prev, next) = db.http_store()?.http_events(&source, paging_type);
+    let mut connection: Connection<String, HttpRawEvent> = Connection::new(prev, next);
+    for http_data in http {
+        let (key, raw_data) = http_data;
+        let de_http = bincode::deserialize::<ingestion::HttpConn>(&raw_data)?;
+        connection
+            .edges
+            .push(Edge::new(base64::encode(key), HttpRawEvent::from(de_http)));
+    }
+    Ok(connection)
+}
+
+fn load_paging_type_rdp(
+    ctx: &Context<'_>,
+    source: &str,
+    after: Option<String>,
+    before: Option<String>,
+    first: Option<usize>,
+    last: Option<usize>,
+) -> Result<Connection<String, RdpRawEvent>> {
+    let db = ctx.data::<Database>()?;
+    let paging_type = check_paging_type(after, before, first, last)?;
+
+    let args: Vec<Vec<u8>> = vec![source.as_bytes().to_vec()];
+    let source = String::from_utf8(gen_key(args))?;
+
+    let (rdp, prev, next) = db.rdp_store()?.rdp_events(&source, paging_type);
+    let mut connection: Connection<String, RdpRawEvent> = Connection::new(prev, next);
+    for rdp_data in rdp {
+        let (key, raw_data) = rdp_data;
+        let de_rdp = bincode::deserialize::<ingestion::RdpConn>(&raw_data)?;
+        connection
+            .edges
+            .push(Edge::new(base64::encode(key), RdpRawEvent::from(de_rdp)));
+    }
+    Ok(connection)
 }
