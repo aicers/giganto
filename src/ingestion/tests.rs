7074d47a95289f4b093771ffeeaf14ccce9dfdaa
Junggon Kim <jgkim@einsis.com>
2022-12-04 16:57:07 +0900

    Add testcode for more data types
    Close: #201

--- src/ingestion/tests.rs
+++ src/ingestion/tests.rs
@@ -468,6 +468,319 @@ async fn periodic_time_series() {
 }
 
 #[tokio::test]
+async fn smtp() {
+    const RECORD_TYPE_SMTP: u32 = 0x06;
+
+    #[derive(Serialize)]
+    struct Smtp {
+        orig_addr: IpAddr,
+        resp_addr: IpAddr,
+        orig_port: u16,
+        resp_port: u16,
+        mailfrom: String,
+        date: String,
+        from: String,
+        to: String,
+        subject: String,
+        agent: String,
+    }
+
+    let _lock = TOKEN.lock().await;
+    let db_dir = tempfile::tempdir().unwrap();
+    let db = Database::open(db_dir.path()).unwrap();
+    tokio::spawn(server().run(db));
+
+    let client = TestClient::new().await;
+    let (mut send_smtp, _) = client.conn.open_bi().await.expect("failed to open stream");
+
+    let mut smtp_data: Vec<u8> = Vec::new();
+    let smtp_body = Smtp {
+        orig_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        resp_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        orig_port: 46378,
+        resp_port: 80,
+        mailfrom: "mailfrom".to_string(),
+        date: "date".to_string(),
+        from: "from".to_string(),
+        to: "to".to_string(),
+        subject: "subject".to_string(),
+        agent: "agent".to_string(),
+    };
+    let mut ser_smtp_body = bincode::serialize(&smtp_body).unwrap();
+
+    smtp_data.append(&mut RECORD_TYPE_SMTP.to_le_bytes().to_vec());
+    smtp_data.append(&mut Utc::now().timestamp_nanos().to_le_bytes().to_vec());
+    smtp_data.append(&mut (ser_smtp_body.len() as u32).to_le_bytes().to_vec());
+    smtp_data.append(&mut ser_smtp_body);
+
+    send_smtp
+        .write_all(&smtp_data)
+        .await
+        .expect("failed to send request");
+
+    send_smtp.finish().await.expect("failed to shutdown stream");
+
+    client.conn.close(0u32.into(), b"smtp_done");
+    client.endpoint.wait_idle().await;
+}
+
+#[tokio::test]
+async fn ntlm() {
+    const RECORD_TYPE_NTLM: u32 = 0x07;
+
+    #[derive(Serialize)]
+    struct Ntlm {
+        orig_addr: IpAddr,
+        resp_addr: IpAddr,
+        orig_port: u16,
+        resp_port: u16,
+        username: String,
+        hostname: String,
+        domainname: String,
+        server_nb_computer_name: String,
+        server_dns_computer_name: String,
+        server_tree_name: String,
+        success: String,
+    }
+
+    let _lock = TOKEN.lock().await;
+    let db_dir = tempfile::tempdir().unwrap();
+    let db = Database::open(db_dir.path()).unwrap();
+    tokio::spawn(server().run(db));
+
+    let client = TestClient::new().await;
+    let (mut send_ntlm, _) = client.conn.open_bi().await.expect("failed to open stream");
+
+    let mut ntlm_data: Vec<u8> = Vec::new();
+    let ntlm_body = Ntlm {
+        orig_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        resp_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        orig_port: 46378,
+        resp_port: 80,
+        username: "bly".to_string(),
+        hostname: "host".to_string(),
+        domainname: "domain".to_string(),
+        server_nb_computer_name: "NB".to_string(),
+        server_dns_computer_name: "dns".to_string(),
+        server_tree_name: "tree".to_string(),
+        success: "tf".to_string(),
+    };
+    let mut ser_ntlm_body = bincode::serialize(&ntlm_body).unwrap();
+
+    ntlm_data.append(&mut RECORD_TYPE_NTLM.to_le_bytes().to_vec());
+    ntlm_data.append(&mut Utc::now().timestamp_nanos().to_le_bytes().to_vec());
+    ntlm_data.append(&mut (ser_ntlm_body.len() as u32).to_le_bytes().to_vec());
+    ntlm_data.append(&mut ser_ntlm_body);
+
+    send_ntlm
+        .write_all(&ntlm_data)
+        .await
+        .expect("failed to send request");
+
+    send_ntlm.finish().await.expect("failed to shutdown stream");
+
+    client.conn.close(0u32.into(), b"ntlm_done");
+    client.endpoint.wait_idle().await;
+}
+
+#[tokio::test]
+async fn kerberos() {
+    const RECORD_TYPE_KERBEROS: u32 = 0x08;
+
+    #[derive(Serialize)]
+    struct Kerberos {
+        orig_addr: IpAddr,
+        resp_addr: IpAddr,
+        orig_port: u16,
+        resp_port: u16,
+        request_type: String,
+        client: String,
+        service: String,
+        success: String,
+        error_msg: String,
+        from: i64,
+        till: i64,
+        cipher: String,
+        forwardable: String,
+        renewable: String,
+        client_cert_subject: String,
+        server_cert_subject: String,
+    }
+
+    let _lock = TOKEN.lock().await;
+    let db_dir = tempfile::tempdir().unwrap();
+    let db = Database::open(db_dir.path()).unwrap();
+    tokio::spawn(server().run(db));
+
+    let client = TestClient::new().await;
+    let (mut send_kerberos, _) = client.conn.open_bi().await.expect("failed to open stream");
+
+    let mut kerberos_data: Vec<u8> = Vec::new();
+    let kerberos_body = Kerberos {
+        orig_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        resp_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        orig_port: 46378,
+        resp_port: 80,
+        request_type: "req_type".to_string(),
+        client: "client".to_string(),
+        service: "service".to_string(),
+        success: "tf".to_string(),
+        error_msg: "err_msg".to_string(),
+        from: 5454,
+        till: 2345,
+        cipher: "cipher".to_string(),
+        forwardable: "forwardable".to_string(),
+        renewable: "renewable".to_string(),
+        client_cert_subject: "client_cert".to_string(),
+        server_cert_subject: "server_cert".to_string(),
+    };
+    let mut ser_kerberos_body = bincode::serialize(&kerberos_body).unwrap();
+
+    kerberos_data.append(&mut RECORD_TYPE_KERBEROS.to_le_bytes().to_vec());
+    kerberos_data.append(&mut Utc::now().timestamp_nanos().to_le_bytes().to_vec());
+    kerberos_data.append(&mut (ser_kerberos_body.len() as u32).to_le_bytes().to_vec());
+    kerberos_data.append(&mut ser_kerberos_body);
+
+    send_kerberos
+        .write_all(&kerberos_data)
+        .await
+        .expect("failed to send request");
+
+    send_kerberos
+        .finish()
+        .await
+        .expect("failed to shutdown stream");
+
+    client.conn.close(0u32.into(), b"kerberos_done");
+    client.endpoint.wait_idle().await;
+}
+
+#[tokio::test]
+async fn ssh() {
+    const RECORD_TYPE_SSH: u32 = 0x09;
+
+    #[derive(Serialize)]
+    struct Ssh {
+        orig_addr: IpAddr,
+        resp_addr: IpAddr,
+        orig_port: u16,
+        resp_port: u16,
+        version: i64,
+        auth_success: String,
+        auth_attempts: i64,
+        direction: String,
+        client: String,
+        server: String,
+        cipher_alg: String,
+        mac_alg: String,
+        compression_alg: String,
+        kex_alg: String,
+        host_key_alg: String,
+        host_key: String,
+    }
+
+    let _lock = TOKEN.lock().await;
+    let db_dir = tempfile::tempdir().unwrap();
+    let db = Database::open(db_dir.path()).unwrap();
+    tokio::spawn(server().run(db));
+
+    let client = TestClient::new().await;
+    let (mut send_ssh, _) = client.conn.open_bi().await.expect("failed to open stream");
+
+    let mut ssh_data: Vec<u8> = Vec::new();
+    let ssh_body = Ssh {
+        orig_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        resp_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        orig_port: 46378,
+        resp_port: 80,
+        version: 01,
+        auth_success: "auth_success".to_string(),
+        auth_attempts: 3,
+        direction: "direction".to_string(),
+        client: "client".to_string(),
+        server: "server".to_string(),
+        cipher_alg: "cipher_alg".to_string(),
+        mac_alg: "mac_alg".to_string(),
+        compression_alg: "compression_alg".to_string(),
+        kex_alg: "kex_alg".to_string(),
+        host_key_alg: "host_key_alg".to_string(),
+        host_key: "host_key".to_string(),
+    };
+    let mut ser_ssh_body = bincode::serialize(&ssh_body).unwrap();
+
+    ssh_data.append(&mut RECORD_TYPE_SSH.to_le_bytes().to_vec());
+    ssh_data.append(&mut Utc::now().timestamp_nanos().to_le_bytes().to_vec());
+    ssh_data.append(&mut (ser_ssh_body.len() as u32).to_le_bytes().to_vec());
+    ssh_data.append(&mut ser_ssh_body);
+
+    send_ssh
+        .write_all(&ssh_data)
+        .await
+        .expect("failed to send request");
+
+    send_ssh.finish().await.expect("failed to shutdown stream");
+
+    client.conn.close(0u32.into(), b"ssh_done");
+    client.endpoint.wait_idle().await;
+}
+
+#[tokio::test]
+async fn dce_rpc() {
+    const RECORD_TYPE_DCE_RPC: u32 = 0x10;
+
+    #[derive(Serialize)]
+    struct DceRpc {
+        orig_addr: IpAddr,
+        resp_addr: IpAddr,
+        orig_port: u16,
+        resp_port: u16,
+        rtt: i64,
+        named_pipe: String,
+        endpoint: String,
+        operation: String,
+    }
+
+    let _lock = TOKEN.lock().await;
+    let db_dir = tempfile::tempdir().unwrap();
+    let db = Database::open(db_dir.path()).unwrap();
+    tokio::spawn(server().run(db));
+
+    let client = TestClient::new().await;
+    let (mut send_dce_rpc, _) = client.conn.open_bi().await.expect("failed to open stream");
+
+    let mut dce_rpc_data: Vec<u8> = Vec::new();
+    let dce_rpc_body = DceRpc {
+        orig_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        resp_addr: "192.168.4.76".parse::<IpAddr>().unwrap(),
+        orig_port: 46378,
+        resp_port: 80,
+        rtt: 3,
+        named_pipe: "named_pipe".to_string(),
+        endpoint: "endpoint".to_string(),
+        operation: "operation".to_string(),
+    };
+    let mut ser_dce_rpc_body = bincode::serialize(&dce_rpc_body).unwrap();
+
+    dce_rpc_data.append(&mut RECORD_TYPE_DCE_RPC.to_le_bytes().to_vec());
+    dce_rpc_data.append(&mut Utc::now().timestamp_nanos().to_le_bytes().to_vec());
+    dce_rpc_data.append(&mut (ser_dce_rpc_body.len() as u32).to_le_bytes().to_vec());
+    dce_rpc_data.append(&mut ser_dce_rpc_body);
+
+    send_dce_rpc
+        .write_all(&dce_rpc_data)
+        .await
+        .expect("failed to send request");
+
+    send_dce_rpc
+        .finish()
+        .await
+        .expect("failed to shutdown stream");
+
+    client.conn.close(0u32.into(), b"dce_rpc_done");
+    client.endpoint.wait_idle().await;
+}
+
+#[tokio::test]
 async fn ack_info() {
     const RECORD_TYPE_LOG: u32 = 0x02;
 
