97d104319f910ca182ffefd8ec29ac9b84dc35d9
sophie <sophie@cluml.com>
2024-12-04 23:44:05 +0900

    Fix `INGEST_VERSION_REQ` and `PUBLISH_VERSION_REQ`

--- src/publish/tests.rs
+++ src/publish/tests.rs
@@ -48,7 +48,7 @@ fn get_token() -> &'static Mutex<u32> {
 }
 
 const CA_CERT_PATH: &str = "tests/certs/ca_cert.pem";
-const PROTOCOL_VERSION: &str = "0.24.0-alpha.1";
+const PROTOCOL_VERSION: &str = "0.23.0";
 
 const NODE1_CERT_PATH: &str = "tests/certs/node1/cert.pem";
 const NODE1_KEY_PATH: &str = "tests/certs/node1/key.pem";
